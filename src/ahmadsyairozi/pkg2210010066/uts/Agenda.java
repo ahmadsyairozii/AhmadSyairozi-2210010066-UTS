c87c7d0e7db38feb837dc70df0f8e69d3dd151c4
ahmadsyairozii <164310700+ahmadsyairozii@users.noreply.github.com>
2024-11-22 18:48:38 +0800

    Menambahkan Java Class untuk Agenda.java

---
+++ src/ahmadsyairozi/pkg2210010066/uts/Agenda.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahmadsyairozi.pkg2210010066.uts;
+
+/**
+ *
+ * @author User
+ */
+public class Agenda {
+    
+}
